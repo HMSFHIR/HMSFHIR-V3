36f4ecb670540e2857a7a551e203e4088c34b51e
Prince Nana  Mensah <burnszephyr520@gmail.com>
2025-07-04 17:52:33 +0000

    Fix URL case sensitivity for dashboard link in request.html

--- Bridge/urls.py
+++ Bridge/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from Patients.views import Dashboard
 
 urlpatterns = [
     #path('inspect/', views.inspect_patient, name='inspect'),
+    path('dashboard/', Dashboard, name='Dashboard'),
 ]
